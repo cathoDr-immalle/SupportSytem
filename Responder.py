74b8ed032de73a6950ba17f27807338e06bd5c69
Driesen Catho <catho.driesen@immalle.eu>
2018-11-19 11:47:01 +0100

    SupportSystem v2

--- Responder.py
+++ Responder.py
@@ -1,3 +1,15 @@
+from random import choice
+
 class Responder:
+    def __init__(self):
+        self.responses = []
+        self.fillResponses()
+
     def generateResponder(self):
-        return "Amaj dat is interssant... Vertel me meer..."
+        return choice(self.responses)
+
+    def fillResponses(self):
+        self.responses.append("Wow!")
+        self.responses.append("Echt?")
+        self.responses.append("leuk!")
+        self.responses.append("cool")
